bc8b6205043aa8e721033196a65b2bfcb581433e
Jayden-58 <jayden.kellar@gmail.com>
2022-09-20 18:47:38 -0700

    Create intro_to_comments.py

---
+++ intro_to_comments.py
@@ -0,0 +1,5 @@
+# this text is a comment.
+# comments are lines of text or code that get ignored when the program runs
+# comments can be started by using '#' 
+# comments are useful for leaving notes to yourself and other programmers who may come across your code.
+#try running this file, notice how nothing happens, this is because this program has only comments, and no code that will actually run.
