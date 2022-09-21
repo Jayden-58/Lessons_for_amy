c952ad22b9435677437795da8fae11c316dc5316
Jayden-58 <jayden.kellar@gmail.com>
2022-09-20 18:53:29 -0700

    Create Intro_to_functions.py

---
+++ Intro_to_functions.py
@@ -0,0 +1,7 @@
+def Announce_function():
+    print("this is a function!")
+
+# notice how this function does nothing when you run it. thats because functions need to be "called" in order to run!
+# uncomment the following line and run the program again
+
+# Announce_function()
