f537eb73d6b29938dfe1646b243816f49a853a9b
Jayden-58 <jayden.kellar@gmail.com>
2022-09-21 10:43:57 -0700

    Update introToVariables.py

--- introToVariables.py
+++ introToVariables.py
@@ -1,9 +1,12 @@
+w = False
 x = 3  # int
 y = "this is a string"  # string
 z = 1.3  # float
 print("hello, these are some basic variables: ")
 # notice how we "cast" the numeric variables to string when printing them!
 # in this case, we're asking the program to treat the numeric variables as if they were strings! The reason why is that python cannot understand print commands that contain multiple types of datatypes
+print(str(w) + "  <-- is a boolean!")
+print(type(w))
 print(str(x) + " is an int!")
 print(type(x))
 print(y + "   <-- that was a string!")
