22ab28a2b8228740cc0f260253ea28d4a9786086
Jayden-58 <jayden.kellar@gmail.com>
2022-09-20 18:26:05 -0700

    Create introToIfElseStatements.py

---
+++ introToIfElseStatements.py
@@ -0,0 +1,8 @@
+print("Hello, please enter if you are 'happy' or 'sad'")
+mood = str(input())
+if (mood == "happy"):
+    print("Yay! You're feeling happy today! :)")
+elif (mood == "sad"):
+    print("Sorry to hear you're sad today. Hope you feel better soon!")
+else:
+    print("That was not one of the choices...")
