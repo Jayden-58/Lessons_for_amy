055d008422ba33804b54eedee91dca889db3e8d7
Jayden-58 <jayden.kellar@gmail.com>
2022-09-20 18:19:42 -0700

    Create addingmachiene.py

---
+++ addingmachiene.py
@@ -0,0 +1,9 @@
+print("Welcome to adding machiene!")
+print("enter the first number you'd like to use.")
+first_number = int(input())
+print("enter the second number you'd like to use.")
+second_number = int(input())
+print("Adding the numbers....")
+sum = first_number + second_number
+print(sum)
+print("thanks for using adding machiene!")
